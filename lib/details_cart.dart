6a0a2183f38a2149deec8381c0c8a1ae5551df60
Felix Tse <felixtse283416@gmail.com>
2024-08-12 13:09:18 -0400

    setup details cart and external dependency for state management

--- lib/details_cart.dart
+++ lib/details_cart.dart
@@ -15,6 +15,25 @@ class DetailsCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(216, 240, 253, 1),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(company, style: Theme.of(context).textTheme.titleMedium),
+          const SizedBox(height: 5),
+          Text(freebie, style: Theme.of(context).textTheme.bodySmall),
+          const SizedBox(height: 5),
+          Text(signup, style: Theme.of(context).textTheme.bodySmall),
+          const SizedBox(height: 5),
+          Text(notes, style: Theme.of(context).textTheme.bodySmall),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/redeem_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:treatme_freebies/redeem_provider.dart';
+
+class RedeemPage extends StatelessWidget {
+  const RedeemPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
